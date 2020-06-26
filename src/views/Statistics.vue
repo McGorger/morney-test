aff37a863f18dee5b170de6c0aee181bedf374db
朱恩光 <764956153@qq.com>
2020-06-26 12:03:56 +0800

    创建三个页面

---
+++ src/views/Statistics.vue
@@ -0,0 +1,19 @@
+<template>
+ <div>
+Statistics
+ </div>
+</template>
+
+<script lang='ts'>
+ import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Statistics extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<style scoped lang='scss'>
+
+ 
+</style>
