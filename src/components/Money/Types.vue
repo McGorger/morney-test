e7bd4b73ca19184dcb9770cbc6303c04a3c100e4
朱恩光 <764956153@qq.com>
2020-06-28 12:41:07 +0800

    安装vue-class-component的文档 使用props失败

---
+++ src/components/Money/Types.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <ul class="types">
+      <li :class="type === '-'&&'selected'" @click="selectType('-')">支出</li>
+      <li :class="type === '+'&&'selected'" @click="selectType('+')">收入</li>
+    </ul>
+  </div>
+</template>
+
+<script lang='ts'>
+import Vue from "vue";
+import { Component, Prop } from 'vue-property-decorator';
+import HelloWorld from '../HelloWorld.vue';
+@Component({
+  props:{
+    propsMessage:String
+  }
+})
+export default class Types extends Vue {
+  type = "-";
+  helloWorld  = 'hellow'+this.propsMessage;
+  selectType(type: string) {
+    if (type !== "-" && type !== "+") {
+      throw new Error("type is unkown");
+    }
+    this.type = type;
+  }
+}
+</script>
+
+<style scoped lang='scss'>
+.types {
+  background: #c4c4c4;
+  display: flex;
+  text-align: center;
+  font-size: 24px;
+
+  > li {
+    width: 50%;
+    line-height: 64px;
+    height: 64px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    &.selected::after {
+      content: "";
+      width: 100%;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      height: 4px;
+      background: #333;
+    }
+  }
+}
+</style>
