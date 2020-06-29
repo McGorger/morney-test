fbc373cbfdaa37137e526a437999f60477b6aef7
朱恩光 <764956153@qq.com>
2020-06-29 14:31:39 +0800

    将tag的string改为{id，name}

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -2,8 +2,8 @@ import Vue from 'vue';
 <template>
   <Layout>
     <ol class="tags">
-      <li v-for="tag in tags" :key="tag">
-        <span>{{ tag }}</span>
+      <li v-for="tag in tags" :key="tag.id">
+        <span>{{ tag.name }}</span>
         <Icon name="right" />
       </li>
     </ol>
